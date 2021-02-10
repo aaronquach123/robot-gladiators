372bfd99afcd2cb585f08be596e723852b186854
aaron <aaronquach12341@gmail.com>
2021-02-10 08:08:02 -0800

    added while loop

--- assets/js/game.js
+++ assets/js/game.js
@@ -1,6 +1,6 @@
 var playerName = window.prompt("What is your robot's name?");
 var playerHealth = 100;
-var playerAttack = 10;
+var playerAttack = 25;
 var playerMoney = 10;
 
 var enemyNames = ["Roborto", "Amy Android", "Robo Trumble"];
@@ -15,7 +15,7 @@ var enemyAttack = 12;
 
 
 var fight = function(enemyName) {
-    window.alert("Welcome to Robot Gladiators!");
+  while(enemyHealth > 0) {
     var promptFight = window.prompt("Would you like to FIGHT or SKIP this battle? Enter 'FIGHT' or 'SKIP' to choose.")
     if (promptFight === "fight" || promptFight === "FIGHT") {
         //Subtract enemy health
@@ -59,8 +59,11 @@ var fight = function(enemyName) {
         } else {
             window.alert("You need to choose a valid option. Try again!");
     }
+  }
 }
 
 for (var i = 0; i < enemyNames.length; i++) {
+    var pickedEnemyName = enemyNames[i];
+    enemyHealth = 50;
     fight(enemyNames[i]);
 }
